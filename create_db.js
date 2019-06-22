ee19309acbf80207cb09a92f5d98c9a92cbf3b0f
alexshi0000 <alexshi0000@gmail.com>
2019-06-22 16:44:06 -0400

    dbs created

--- create_db.js
+++ create_db.js
@@ -1,9 +1,9 @@
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/foody";
+var url = "mongodb://127.0.0.1:27017/foody";
 
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
-  console.log("foody app db created!");
+  console.log("foody app db connected!");
 
   var dbo = db.db("foody");
 
