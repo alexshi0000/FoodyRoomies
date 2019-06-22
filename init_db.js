98ea2ac4d5f808eadd23c971d6aa38a0d5818b70
alexshi0000 <alexshi0000@gmail.com>
2019-06-22 14:55:16 -0400

    .

--- init_db.js
+++ init_db.js
@@ -1,25 +1,44 @@
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/marketplace";
+var url = "mongodb://localhost:27017/foody";
 
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
-  console.log("Marketplace created!");
+  console.log("foody app db created!");
 
-  var dbo = db.db("marketplace");
+  var dbo = db.db("foody");
 
-  var callback = () => {
-    dbo.createCollection('shoppingCart', (err, res) => {
+  //callback hell here, do not enter this zone
+  var create_fridge_collection = () => {
+    dbo.createCollection("fridge", (err, res) => {
       if (err)
         throw err;
-      console.log('shopping cart listing created!');
-      db.close();
+      console.log('fridge collection created!');
+    });
+    db.close();
+  };
+
+  var create_expenses_collection = () => {
+    dbo.createCollection("expenses", (err, res) => {
+      if (err)
+        throw err;
+      console.log('expenses collection created!');
+      create_fridge_collection();
+    });
+  };
+
+  var create_groceries_collection = () => {
+    dbo.createCollection("groceries", (err, res) => {
+      if (err)
+        throw err;
+      console.log('groceries collection created!');
+      create_expenses_collection();
     })
   };
 
-  dbo.createCollection("products", function(err, res) {
+  dbo.createCollection("chores", function(err, res) {
     if (err)
       throw err;
-    console.log("product collection created!");
-    callback();
+    console.log("chores collection created!");
+    create_groceries_collection();
   });
 });
